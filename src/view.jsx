5a94585a506610f3aae0d9717edfd64dbc5ae29b
Liang Xingchen <liang@maichong.it>
2016-04-07 19:34:49 +0800

    0.7.0

--- src/view.jsx
+++ src/view.jsx
@@ -8,20 +8,57 @@ import React from 'react';
 
 import Select from './Select';
 import { shallowEqual } from 'alaska-admin-view';
+import _ from 'lodash';
+
+function filter(options, data) {
+  if (!options || !data || !options.length) {
+    return options;
+  }
+  let result = [];
+  options.forEach(opt => {
+    if (!opt.depends) {
+      result.push(opt);
+      return;
+    }
+    if (typeof opt.depends === 'string') {
+      if (data[opt.depends]) {
+        result.push(opt);
+      }
+    } else if (typeof opt.depends === 'object' && _.every(opt.depends, (value, k) => data[k] === value)) {
+      result.push(opt);
+    }
+  });
+  return result;
+}
 
 export default class SelectFieldView extends React.Component {
 
   static propTypes = {
-    children: React.PropTypes.node
+    field: React.PropTypes.object,
+    data: React.PropTypes.object,
+    onChange: React.PropTypes.func,
+    value: React.PropTypes.any,
+    disabled: React.PropTypes.bool,
+    errorText: React.PropTypes.string,
   };
 
-  static contextTypes = {
-    views: React.PropTypes.object,
-    settings: React.PropTypes.object,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      options: filter(props.field.options, props.data)
+    };
+  }
 
-  shouldComponentUpdate(props) {
-    return !shallowEqual(props, this.props, 'data', 'onChange', 'model');
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.field !== this.props.field || nextProps.data !== this.props.data) {
+      this.setState({
+        options: filter(nextProps.field.options, nextProps.data)
+      });
+    }
+  }
+
+  shouldComponentUpdate(props, state) {
+    return !shallowEqual(props, this.props, 'data', 'onChange', 'model') || state !== this.state;
   }
 
   handleChange = (option) => {
@@ -44,7 +81,7 @@ export default class SelectFieldView extends React.Component {
           <Select
             value={value}
             disabled={disabled}
-            options={field.options}
+            options={this.state.options}
             onChange={this.handleChange}
           />
           {helpElement}
